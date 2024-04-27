5e33c3e94ca0435c884d7793ec15920d60c6cdfa
Satvik Ajay Kambli <147248804+satvik231@users.noreply.github.com>
2024-04-27 19:23:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var con = document.querySelector("#container")
+var icon = document.querySelector("i")
+
+con.addEventListener("dblclick",()=>{
+    icon.style.transform = 'translate(-50%,-50%) scale(1)'
+    icon.style.opacity= 0.8;
+    setTimeout(()=>{
+    icon.style.opacity= 0;
+    },1000)
+    setTimeout(()=>{
+    icon.style.transform = 'translate(-50%,-50%) scale(0)'
+    },3000)
+})
